8fb9b7cb44b0095fae7a514d4cf3625bd3865d7a
Matt Hoback <matthoback@gmail.com>
2021-12-06 21:48:18 -0800

    Day 7

--- PowerShell/2021/Day 07/day07part2.ps1
+++ PowerShell/2021/Day 07/day07part2.ps1
@@ -3,15 +3,16 @@ $in = Get-Content .\input.txt
 $positions = $in.Split(",") | % { [int]$_ }
 
 $maxposition = ($positions | Measure-Object -Maximum).Maximum
-$minfuel = $maxposition * $positions.Count
+$minfuel = $maxposition * $positions.Count * $positions.Count
 $minposition = 0;
 
 for ($i = 0; $i -le $maxposition; $i++) {
     $totalfuel = 0
     for ($j = 0; $j -lt $positions.Count; $j++) {
-        $totalfuel += [Math]::Abs($positions[$j] - $i)
+        $distance = [Math]::Abs($positions[$j] - $i)
+        $totalfuel += ($distance * ($distance + 1))/2
     }
     if ($totalfuel -lt $minfuel) { $minfuel = $totalfuel; $minposition = $i }
 }
 
-Write-Output ("Part 1: " + $minfuel + " , " + $minposition)
+Write-Output ("Part 2: " + $minfuel + " , " + $minposition)
