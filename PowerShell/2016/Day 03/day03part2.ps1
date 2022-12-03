07cf86934f3309f4b860a40dd90f1ccf9c11a427
Matt Hoback <matthoback@gmail.com>
2022-12-02 21:22:11 -0800

    2016 Day 3

---
+++ PowerShell/2016/Day 03/day03part2.ps1
@@ -0,0 +1,11 @@
+$in = Get-Content "$PSScriptRoot\input.txt"
+
+$count = 0
+for ($i = 0; $i -lt $in.Count; $i += 3) {
+    $sides = $in[$i] -split '\s+' | ? { $_ -ne '' } | % { [int]$_ }
+    if ( ($sides[0] + $sides[1] -gt $sides[2]) -and 
+         ($sides[0] + $sides[2] -gt $sides[1]) -and 
+         ($sides[1] + $sides[2] -gt $sides[0])     ) { $count++ }
+}
+
+Write-Host $count
